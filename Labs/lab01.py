f458317bc9c9965b011cc14236757aba2c364995
loganrudd <loganrudd@gmail.com>
2018-05-28 15:37:46 -0700

    Create lab01.py

---
+++ Labs/lab01.py
@@ -0,0 +1,19 @@
+def my_email():
+    """Return your email address as a string.
+
+    >>> my_email() != 'oski@berkeley.edu'
+    True
+    """
+    return 'loganrudd@berkeley.edu'
+
+from operator import add, mul
+
+def twenty_fourteen():
+    """Come up with the most creative expression that evaluates to 2014, 
+    using only numbers and the functions add(. . .) and mul(. . .).
+
+    >>> twenty_fourteen()
+    2014
+    """
+    "*** YOUR CODE HERE ***"
+    return mul(2,add(1000,7))
