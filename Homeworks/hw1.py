90cc60acb6d0cf6d7a3b5290b05c823a247f4eea
loganrudd <loganrudd@gmail.com>
2018-05-28 15:40:49 -0700

    Create hw1.py

---
+++ Homeworks/hw1.py
@@ -0,0 +1,106 @@
+from operator import add, sub
+
+def a_plus_abs_b(a, b):
+    """Return a+abs(b), but without calling abs.
+
+    >>> a_plus_abs_b(2, 3)
+    5
+    >>> a_plus_abs_b(2, -3)
+    5
+    """
+    if b < 0:
+        f = sub
+    else:
+        f = add
+    return f(a, b)
+
+def two_of_three(a, b, c):
+    """Return x*x + y*y, where x and y are the two largest members of the
+    positive numbers a, b, and c.
+
+    >>> two_of_three(1, 2, 3)
+    13
+    >>> two_of_three(5, 3, 1)
+    34
+    >>> two_of_three(10, 2, 8)
+    164
+    >>> two_of_three(5, 5, 5)
+    50
+    """
+    "*** YOUR CODE HERE ***"
+    if a<=b and a<=c:
+        return add(b*b,c*c)
+    if b<=a and b<=c:
+        return add(a*a,c*c)
+    if c<=a and c<=b:
+        return add(a*a,b*b)
+
+def if_function(condition, true_result, false_result):
+    """Return true_result if condition is a true value, and
+    false_result otherwise.
+
+    >>> if_function(True, 2, 3)
+    2
+    >>> if_function(False, 2, 3)
+    3
+    >>> if_function(3==2, 3+2, 3-2)
+    1
+    >>> if_function(3>2, 3+2, 3-2)
+    5
+    """
+    if condition:
+        return true_result
+    else:
+        return false_result
+
+def with_if_statement():
+    """
+    >>> with_if_statement()
+    1
+    """
+    if c():
+        return t()
+    else:
+        return f()
+
+def with_if_function():
+    return if_function(c(), t(), f())
+
+def c():
+    "*** YOUR CODE HERE ***"
+    return True
+def t():
+    "*** YOUR CODE HERE ***"
+    return 1
+def f():
+    "*** YOUR CODE HERE ***"
+    return 1/0
+def hailstone(n):
+    """Print the hailstone sequence starting at n and return its
+    length.
+
+    >>> a = hailstone(10)
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    >>> a
+    7
+    """
+    "*** YOUR CODE HERE ***"
+    i=1
+    while n>1:
+        i+=1
+        print(n)
+        if n%2==0:
+            n=n//2
+        else:
+            n=n*3+1
+    print(n)
+    return i
+challenge_question_program = """
+"*** YOUR CODE HERE ***"
+"""
